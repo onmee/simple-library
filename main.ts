557b7d880d8d05237f2b3a624642654ede1e09d6
onmee <48259011+onmee@users.noreply.github.com>
2023-11-26 15:44:36 +0000

    Dynamically generate book cards

--- main.ts
+++ main.ts
@@ -67,14 +67,66 @@ function addBookToLibrary(e) {
 }
 
 // Create card for newly added book
-function bookCard() {
+function newBookCard() {
+    const main = document.querySelector("main");
+    const card = document.createElement("div");
+    const span = document.createElement("span");
+    const header = document.createElement("div");
+    const info = document.createElement("div");
+    const author = document.createElement("div");
+    const authorName = document.createElement("div");
+    const pages = document.createElement("div");
+    const pagesNum = document.createElement("div");
+    const genre = document.createElement("div");
+    const genreName = document.createElement("div");
+    const status = document.createElement("div");
+    const statusShown = document.createElement("div");
+    const statusBtn = document.createElement("button")
+
+    span.classList.add('remove');
+    span.innerHTML = '<img src="assets/close.svg" alt="">';
+    header.classList.add('book-header');
+    header.textContent = 'Lord of the Rings';
+    info.classList.add('info-grid');
+    author.classList.add('info-title');
+    author.textContent = 'Author';
+    authorName.classList.add('info');
+    authorName.textContent = 'J.R.R Tolkien';
+    pages.classList.add('info-title');
+    pages.textContent = 'Pages';
+    pagesNum.classList.add('info');
+    pagesNum.textContent = '230';
+    genre.classList.add('info-title');
+    genre.textContent = 'Genre';
+    genreName.classList.add('info');
+    genreName.textContent = 'Fantasy';
+    status.classList.add('info-title');
+    status.textContent = 'Status';
+    statusShown.classList.add('info');
+    statusShown.textContent = 'Read';
+    statusBtn.classList.add('status');
+    statusBtn.textContent = 'Change Status';
+
+    card.classList.add('book-card');
     
+    card.appendChild(span);
+    card.appendChild(header);
+    card.appendChild(info);
+    info.appendChild(author);
+    info.appendChild(authorName);
+    info.appendChild(pages);
+    info.appendChild(pagesNum);
+    info.appendChild(genre);
+    info.appendChild(genreName);
+    info.appendChild(status);
+    info.appendChild(statusShown);
+    card.appendChild(statusBtn);
+    main.append(card);
 } 
-// Loop through the library array and add the data to the cards
 
-
-function validateForm() {
-
+// Delete any book card
+function deleteBookCard() {
+    
 }
 
 //Code reasoning
